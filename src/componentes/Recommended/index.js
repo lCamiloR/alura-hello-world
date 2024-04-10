275278fd9a98ac6e96411d17bae9fb2146cdb878
Luccas Camilo Roberto <61637025+lCamiloR@users.noreply.github.com>
2024-04-10 17:23:34 -0300

    Update index.js

--- src/componentes/Recommended/index.js
+++ src/componentes/Recommended/index.js
@@ -3,21 +3,20 @@ import posts from 'json/posts.json';
 import PostCard from "componentes/PostCard";
 
 export default function Recommended({postId}) {
-    let counter = 0;
+    let filteredPosts = posts.filter(p => p.id != postId);
+    filteredPosts.sort(function (a, b) {
+        return a - b;
+    });
+    filteredPosts = filteredPosts.slice(0, 4);
     return (
         <div className={styles.recommended}>
             <h1>Outros posts que você pode gostar:</h1>
             <ul className={styles.posts}>
-                {posts.map(post => {
-                    if (counter < 4 && postId != post.id) {
-                        counter++;
-                        return(
-                            <li key={post.id}>
-                                <PostCard post={post} />
-                            </li>
-                        )
-                    }
-                })}
+                {filteredPosts.map(post => (
+                    <li key={post.id}>
+                        <PostCard post={post} />
+                    </li>
+                ))}
             </ul>
         </div>
     )
